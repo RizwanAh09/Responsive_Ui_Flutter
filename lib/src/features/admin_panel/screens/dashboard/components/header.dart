50f758c63f28b19c7f634724a468e912c4c5b54c
Rizwan <ahrizwan81@gmail.com>
2022-11-26 16:17:22 +0530

    first commit

--- lib/src/features/admin_panel/screens/dashboard/components/header.dart
+++ lib/src/features/admin_panel/screens/dashboard/components/header.dart
@@ -24,7 +24,7 @@ class Header extends StatelessWidget {
         if (!Responsive.isMobile(context))
           Spacer(flex: Responsive.isDesktop(context) ? 2 : 1),
         const Expanded(child: SearchField()),
-        const ProfileCard()
+        const ProfileCard(),
       ],
     );
     //
